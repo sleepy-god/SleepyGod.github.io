2fc4fd8ab5b115f10f0cf56c404c86652ebcdd26
sleepy-god <1402168918+sleepy-god@users.noreply.github.com>
2020-06-12 09:18:36 +0800

    Site updated: 2020-06-12 09:18:29

--- js/main.js
+++ js/main.js
@@ -910,16 +910,20 @@ $(function () {
   }
 
   if (GLOBAL_CONFIG.baiduPush) {
-    (function () {
-      var bp = document.createElement('script')
-      var curProtocol = window.location.protocol.split(':')[0]
-      if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js'
-      } else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js'
-      }
-      var s = document.getElementsByTagName('script')[0]
-      s.parentNode.insertBefore(bp, s)
-    })()
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+
   }
 })
